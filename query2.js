5196b3b5b6061b4b9855fa9ed66c3f2c122e7659
Luke McGuinness <lukemcg@umich.edu>
2025-03-17 22:44:44 -0400

    query 3 in progress...Q1 and Q2 done

--- query2.js
+++ query2.js
@@ -9,7 +9,17 @@
 function unwind_friends(dbname) {
     db = db.getSiblingDB(dbname);
 
-    // TODO: unwind friends
+    db.users.aggregate([
+        //Unwinding friends vv
+        { $unwind: "$friends" },
+        { $project: { _id: false, user_id: true, friends: "$friends" } }, // creates collection with req fields
+        { $out: "flat_users" } // output to "flat_users"
+    ]);
 
     return;
 }
+
+// // { $project: { <specification(s)> } }
+// <field>: <1 or true> Specifies the inclusion of a field. Non-zero integers are also treated as true.
+
+// _id: <0 or false> Specifies the suppression of the _id field. To exclude a field conditionally, use the REMOVE variable instead. For details, see Exclude Fields Conditionally.
